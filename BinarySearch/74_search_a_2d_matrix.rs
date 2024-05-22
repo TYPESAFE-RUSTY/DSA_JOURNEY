aad12b18d4982241ef37f27903b44e1ee45414b0
TYPESAFE-RUSTY <155160359+TYPESAFE-RUSTY@users.noreply.github.com>
2024-05-23 00:39:27 +0530

    question completed !

--- BinarySearch/74_search_a_2d_matrix.rs
+++ BinarySearch/74_search_a_2d_matrix.rs
@@ -1,1 +1,26 @@
-// working on it  (╥_╥)
+impl Solution {
+    pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
+        let mut rlow = 0;
+        let mut rhigh = matrix.len();
+
+        while rlow < rhigh {
+            let mid = (rlow + rhigh) / 2;
+            if matrix[mid][0] <= target && matrix[mid].last().unwrap() >= &target {
+                match matrix[mid].binary_search(&target) {
+                    Ok(_) => {
+                        return true;
+                    }
+                    _ => {
+                        return false;
+                    }
+                }
+            } else if matrix[mid][0] > target {
+                rhigh = mid;
+            } else {
+                rlow = mid + 1;
+            }
+        }
+
+        false
+    }
+}
